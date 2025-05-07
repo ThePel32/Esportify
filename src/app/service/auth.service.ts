a5c747a7fa93c6f93b362e52ee45537241ce8efa
ThePel <theo.pelven32@gmail.com>
2025-05-07 18:20:42 +0200

    fix environment

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
-import { environment } from '../../environments/environments';
+import { environment } from '../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
