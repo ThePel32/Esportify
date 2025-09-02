85c0fe9be8712a3603f1281a6df68876cecc21b0
ThePel <theo.pelven32@gmail.com>
2025-09-02 15:18:41 +0200

    connexion mongoose

--- esportify-backend/app/config/mongo.js
+++ esportify-backend/app/config/mongo.js
@@ -1,9 +1,8 @@
-const mongoose = require("mongoose");
-const { URI } = require("./mongo.config");
+const mongoose = require('mongoose');
+const { URI } = require('./mongo.config');
 
-mongoose
-    .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("MongoDB connécté avec succès"))
-    .catch(err => console.error("Erreur de connexion à MongoDB:", err));
+mongoose.connect(URI)
+    .then(() => console.log('MongoDB connecté avec succès'))
+    .catch((err) => console.error('Erreur de connexion à MongoDB:', err));
 
 module.exports = mongoose;
